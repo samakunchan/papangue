e18ac984ad5e4886d710326ac768ae3b77cd6007
samakunchan <badgehades@hotmail.com>
2023-07-23 12:29:51 +0200

    feat: clean code

--- src/app/core/utils/initializer.ts
+++ src/app/core/utils/initializer.ts
@@ -7,20 +7,20 @@ import { IFreelanceApi } from '../interfaces/freelance-api.interface';
  * @description Factory qui sert à detecter si les API sont actives
  * @description Initialiser dans AppModule
  * @param {HttpClient} httpClient
- * @return {() => Observable<[Object, Object]>}
+ * @return {() => Observable<[IFreelanceApi]>}
  * @author Samakunchan
  * @example ```ts
- *   providers: [
- *     {
- *       provide: APP_INITIALIZER,
- *       useFactory: dectectionApiReady,
- *       deps: [HttpClient],
- *       multi: true,
- *     },
- *  ]
+ providers: [
+ {
+         provide: APP_INITIALIZER,
+         useFactory: detectionApisReady,
+         deps: [HttpClient],
+         multi: true,
+       },
+ ]
  * ```
  */
-export const dectectionApiReady = (httpClient: HttpClient): (() => Observable<[IFreelanceApi]>) => {
+export const detectionApisReady = (httpClient: HttpClient): (() => Observable<[IFreelanceApi]>) => {
   return (): Observable<[IFreelanceApi]> =>
     combineLatest([httpClient.get<IFreelanceApi>(`${environment.apiUrl}`)]).pipe(
       tap(() => console.log(`Initialisation de l'app en %c${window.performance.now() / 1000}s`, 'color: #2780e1')),
