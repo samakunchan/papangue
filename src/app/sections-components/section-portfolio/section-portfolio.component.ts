3c6585b8c069624929ade0a303139378a162577a
samakunchan <badgehades@hotmail.com>
2023-07-31 09:36:45 +0200

    feat: animation
    - Création d'un fichier pour les animations en angular pure
    - Suppression du fichier main.js
    - Suppression de la dépendance aos.js

--- src/app/sections-components/section-portfolio/section-portfolio.component.ts
+++ src/app/sections-components/section-portfolio/section-portfolio.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { ISection } from '../../core/interfaces/section.interface';
+import { RouteName } from '../../core/utils/config.core';
+import { transition, trigger } from '@angular/animations';
+import { showElementWith300msDelay, startByHideElement } from '../../core/utils/animations';
 
 @Component({
   selector: 'app-section-portfolio',
   templateUrl: './section-portfolio.component.html',
   styleUrls: ['./section-portfolio.component.scss'],
+  animations: [trigger('thumbState', [transition('void => *', [startByHideElement, showElementWith300msDelay])])],
 })
 export class SectionPortfolioComponent {
   @Input('section') section!: ISection;
